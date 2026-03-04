e79e26ff8e98b4fb97049bb794c04b640e93882e
craignim1609-pixel <craignim1609@gmail.com>
2026-03-04 11:19:58 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app import models
 from app.routers import picks, accumulator, stats, raceday, export
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -17,3 +22,12 @@ app.include_router(export.router)
 @app.get("/")
 def home():
     return {"message": "Horse Racing Tracker API running"}
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/home", response_class=HTMLResponse)
+def home_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
