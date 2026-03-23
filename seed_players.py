d8ad6b3d0e95cecb3fa9fd44cab0b67b9e312e66
craignim1609-pixel <craignim1609@gmail.com>
2026-03-23 22:02:28 +0000

    Update seed_players.py

--- seed_players.py
+++ seed_players.py
@@ -1,19 +1,15 @@
-from app.database import SessionLocal, engine
-from app.models import Base, Player
+from app.database import SessionLocal
+from app.models import Player
 
-# Create tables if they don't exist
-Base.metadata.create_all(bind=engine)
+def seed_players():
+    db = SessionLocal()
 
-db = SessionLocal()
+    players = ["Donald", "Miller", "Nick", "Josh", "Craig"]
 
-players = ["Donald", "Miller", "Nick", "Josh", "Craig"]
+    for name in players:
+        exists = db.query(Player).filter(Player.name == name).first()
+        if not exists:
+            db.add(Player(name=name))
 
-for name in players:
-    exists = db.query(Player).filter(Player.name == name).first()
-    if not exists:
-        db.add(Player(name=name))
-
-db.commit()
-db.close()
-
-print("Players inserted successfully.")
+    db.commit()
+    db.close()
