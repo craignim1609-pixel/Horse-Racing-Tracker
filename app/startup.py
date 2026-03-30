acee961c511551eefd09a8e60d204327b119a608
craignim1609-pixel <craignim1609@gmail.com>
2026-03-30 07:49:04 +0100

    Update startup.py

--- app/startup.py
+++ app/startup.py
@@ -1,15 +1,35 @@
 from app.database import SessionLocal
 from app import models
 
+
 def seed_players():
+    """
+    Seeds the database with default players if they do not exist.
+    Safe to run multiple times.
+    """
     db = SessionLocal()
 
-    default_players = ["Craig", "Donald", "Miller", "Nick", "Josh"]
+    try:
+        default_players = ["Craig", "Donald", "Miller", "Nick", "Josh"]
 
-    for name in default_players:
-        exists = db.query(models.Player).filter(models.Player.name == name).first()
-        if not exists:
-            db.add(models.Player(name=name))
+        for name in default_players:
+            exists = (
+                db.query(models.Player)
+                .filter(models.Player.name == name)
+                .first()
+            )
+            if not exists:
+                db.add(models.Player(name=name))
 
-    db.commit()
-    db.close()
+        db.commit()
+
+    finally:
+        db.close()
+
+
+def run_startup_tasks():
+    """
+    Called from main.py on startup.
+    Add more startup tasks here later.
+    """
+    seed_players()
