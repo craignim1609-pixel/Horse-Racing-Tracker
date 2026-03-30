661ce3d0f2ad8c18a358c5bda4568edf85a5b78b
craignim1609-pixel <craignim1609@gmail.com>
2026-03-30 14:42:56 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.include_router(picks.router)
 app.include_router(accumulator.router)
 #app.include_router(stats.router)
 app.include_router(raceday.router)
-app.include_router(export.router)
+#app.include_router(export.router)
 #app.include_router(players.router)
 
 import os
