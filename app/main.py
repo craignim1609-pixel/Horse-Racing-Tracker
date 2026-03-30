e8566fbefe3adcd893086d71db756dec1d0269f0
craignim1609-pixel <craignim1609@gmail.com>
2026-03-30 14:42:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app import models
-from app.routers import picks, accumulator, stats, raceday, export, players
+from app.routers import picks, accumulator, raceday
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
