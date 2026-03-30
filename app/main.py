f65e1e1e180ebd5aac9789e1ccd91c8130e59b84
craignim1609-pixel <craignim1609@gmail.com>
2026-03-30 09:27:40 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
 
 from app.routers.picks import router as picks_router
 from app.routers.accumulator import router as acca_router
