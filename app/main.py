e48ba0d50d2e6c0f8926d54199387a4d478f9d1b
craignim1609-pixel <craignim1609@gmail.com>
2026-03-31 07:01:31 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from app.routers.players import seed_players
+from app.routers import players
 
 app = FastAPI()
 
